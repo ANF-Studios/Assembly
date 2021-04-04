36a399b4c48e5f24f9d8e44ed1a3a8ce6e1fbd94
ANF-Studios <theanfstudios@gmail.com>
2021-04-04 13:46:15 -0400

    Update

--- main.asm
+++ main.asm
@@ -1,26 +1,37 @@
-global  _main
-extern  _printf
+global _main
+extern _printf
 
 section .data
-a:
-    dd      2
-b:
-    dd      1
-c:
-    resd    1
-msg:
-    db "%i", 10, 0 ; "%08X" for a full int.
+a:  dd    65
+fmt: db "%i", 10, 0
 
 section .text
 _main:
-    mov      eax, [a]
-    sub      eax, [b]
-    mov      [c], eax
-    mov      ebx, [c]
-    push     ebx
-    push     msg
-    call     _printf
-    add      esp, 8
+    ;mov     eax, [a]
+    ;push    eax
+    ;push    fmt
+    ;push    1337
+    ;push    fmt
+    ;push    eax
+    ;call    _printf
+    ;pop     eax
+    ;add     esp, 8
+    ;ret
+    ;call    foo
+    call     bar
     ret
-message:
-    db  'Hello World!', 10, 0
+
+bar:
+    
+
+foo:
+    mov     eax, [a]
+    push    eax
+    push    fmt
+    call    _printf
+    ;add     esp, 8
+    add     esp, 4
+    mov     ebx, fmt
+    pop     eax
+    pop     ebx
+    ret
