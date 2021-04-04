8268dd94a27a92aa76e3d05875138002aba2e9c2
ANF-Studios <theanfstudios@gmail.com>
2021-04-04 15:16:33 -0400

    Update

--- main.asm
+++ main.asm
@@ -1,37 +1,10 @@
 global _main
 extern _printf
+extern _getch
 
 section .data
-a:  dd    65
-fmt: db "%i", 10, 0
+    a:  dd    65
+    fmt: db "%i", 10, 0
 
 section .text
 _main:
-    ;mov     eax, [a]
-    ;push    eax
-    ;push    fmt
-    ;push    1337
-    ;push    fmt
-    ;push    eax
-    ;call    _printf
-    ;pop     eax
-    ;add     esp, 8
-    ;ret
-    ;call    foo
-    call     bar
-    ret
-
-bar:
-    
-
-foo:
-    mov     eax, [a]
-    push    eax
-    push    fmt
-    call    _printf
-    ;add     esp, 8
-    add     esp, 4
-    mov     ebx, fmt
-    pop     eax
-    pop     ebx
-    ret
