1590102f2a3bc0f5624be4ae1ec710b8f4fc7b71
ANF-Studios <theanfstudios@gmail.com>
2021-04-04 08:35:47 -0400

    Added explaination comments

--- collections/hello_world.asm
+++ collections/hello_world.asm
@@ -1,11 +1,11 @@
-global    _main
-extern    _printf
+global    _main   ; Define the main function.
+extern    _printf ; Import an external function.
 
 segment  .data
-        message: db   'Hello world', 0xA, 0
+        message: db   'Hello world', 0xA, 0 ; Create a variable called "message"
 section .text
 _main:
-        push    message
-        call    _printf
-        add     esp, 4
+        push    message ; Decrement the stack pointer and then stores the source operand on the top of the stack.
+        call    _printf ; Call the printf function with "message" as the argument.
+        add     esp, 4  ; Clear the esp register by 4 bytes.
         ret
