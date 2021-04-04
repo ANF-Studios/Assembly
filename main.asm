a64a946a3924e1af29bcc836510425c7928292c8
ANF-Studios <theanfstudios@gmail.com>
2021-04-04 10:32:21 -0400

    Removed unnecessary code parts

--- main.asm
+++ main.asm
@@ -3,29 +3,23 @@ extern  _printf
 
 section .data
 a:
-    dd      1
+    dd      2
 b:
     dd      1
 c:
     resd    1
-;fmt:
-;    db "c=%ld, eax=%ld", 10, 0
 msg:
     db "%i", 10, 0 ; "%08X" for a full int.
 
 section .text
 _main:
-    ;push    message
-    ;call    _printf
-    ;add     esp, 4
     mov      eax, [a]
-    add      eax, [b]
+    sub      eax, [b]
     mov      [c], eax
     mov      ebx, [c]
     push     ebx
     push     msg
     call     _printf
-    ;push     [c] ; main.asm:20: error: operation size not specified
     add      esp, 8
     ret
 message:
