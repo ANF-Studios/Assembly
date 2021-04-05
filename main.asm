cdaa34178e60ea516f8d3ecd36db3c465b5ac6cd
ANF-Studios <theanfstudios@gmail.com>
2021-04-05 11:16:52 -0400

    Update

--- main.asm
+++ main.asm
@@ -3,19 +3,18 @@ extern _printf
 extern _getch
 
 section .data
-    x:  dd 5
-    y:  dd 7
+    x:  dd 9
+    y:  dd 3
     fmt:  db "%i", 10, 0 
 
 section .text
 _main:
-    mov      eax, [x]
-    mov      ebx, [y]
-    imul     eax, ebx
-    mov      ecx, eax
-    push     ecx
+    mov      eax, [x] ; numerator, and later result
+    mov      ecx, [y] ; denominator
+    mov      edx, 0   ; remainder
+    idiv     ecx      ; divide
+    push     eax
     push     fmt
     call     _printf
-    pop      ecx
-    add      esp, 4
+    add      esp, 8
     ret
