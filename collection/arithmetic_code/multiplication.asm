a3802d82329b562fc4928cd9966a08b002290e83
ANF-Studios <theanfstudios@gmail.com>
2021-04-05 10:05:02 -0400

    Added multiplication.asm

---
+++ collection/arithmetic_code/multiplication.asm
@@ -0,0 +1,20 @@
+global _main
+extern _printf
+
+section .data
+    x:  dd 5
+    y:  dd 7
+    fmt:  db "%i", 10, 0 
+
+section .text
+_main:
+    mov      eax, [x]
+    mov      ebx, [y]
+    imul     eax, ebx
+    mov      ecx, eax
+    push     ecx
+    push     fmt
+    call     _printf
+    pop      ecx
+    add      esp, 4
+    ret
