52a468c5d6ad3f59d0cabf912080f33d8a33a832
ANF-Studios <theanfstudios@gmail.com>
2021-04-26 00:00:21 -0400

    Added Franklin 4 VR MMO v1.0.1 code

---
+++ collection/other/Franklin4VRMMO.asm
@@ -0,0 +1,62 @@
+; Franklin 4 VR MMO (Unofficial, Fan Edition) v1.0.1
+; Distributed on MIT License, by ANF-Studios.
+;
+; It's a fun silly idea, thanks to ibra (on GitHub)
+; for giving me this idea!
+
+global _main
+extern _printf
+extern _getch
+
+section .data
+    one:
+        db  "Welcome to Franklin!", 10, 0
+    two:
+        db  "Franklin is playing a VR MMO game...", 10, 0
+    three:
+        db  "He then gets captured and gets sent to the void...", 10, 0
+    four:
+        db  "He finds a button there...", 10, 0
+    five:
+        db  "He clicks that button and returns home safely...", 10, 0
+    six:
+        db  "That's it.. you can go now, story ended, bye!", 0
+
+section .text
+    _main:
+        mov     eax, one
+        push    eax
+        call    _printf
+        call    _getch
+
+        mov     ebx, two
+        push    ebx
+        call    _printf
+        pop     ebx
+        call    _getch
+
+        mov     ecx, three
+        push    ecx
+        call    _printf
+        pop     ecx
+        call    _getch
+
+        mov     edx, four
+        push    edx
+        call    _printf
+        pop     edx
+        call    _getch
+
+        mov     edx, five
+        push    edx
+        call    _printf
+        pop     edx
+        call    _getch
+
+        mov     eax, six
+        push    eax
+        call    _printf
+        add     esp, 8
+        call    _getch
+
+        ret
